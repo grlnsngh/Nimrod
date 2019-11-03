cd30582df9c14d259caa1768bd0eac9babf5d0e9
Gurleen Singh <grlnsngh@gmail.com>
2019-11-02 23:08:49 -0400

    login Page intent working successfully

--- app/src/main/java/com/example/nimrod/MainActivity.java
+++ app/src/main/java/com/example/nimrod/MainActivity.java
@@ -11,6 +11,7 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
 
 public class MainActivity extends AppCompatActivity {
 
