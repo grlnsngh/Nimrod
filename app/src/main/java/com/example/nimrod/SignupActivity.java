66399831ba3b2739a8911c0262136a6d57c77b6e
Gurleen Singh <grlnsngh@gmail.com>
2019-11-02 22:45:47 -0400

    login Page intent working successfully

--- app/src/main/java/com/example/nimrod/SignupActivity.java
+++ app/src/main/java/com/example/nimrod/SignupActivity.java
@@ -1,6 +1,7 @@
 package com.example.nimrod;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,7 +11,12 @@ public class SignupActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        //Hide Action Bar
+        try {
+            this.getSupportActionBar().hide();
+        } catch (NullPointerException e) {
+            Toast.makeText(this, e.toString(), Toast.LENGTH_SHORT).show();
+        }
         setContentView(R.layout.signup_screen);
     }
 }
